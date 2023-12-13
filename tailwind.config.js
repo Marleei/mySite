8a0d41463db0ebd672b4b492624020b717f29f91
Marleei <Selenium99@yandex.ru>
2023-12-13 16:24:31 +0300

    add README

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const colors= require('tailwindcss/colors')
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    fontFamily:{
+      'body':'Space Ranger_Rus,sans-serif'
+    },
+    container: {
+      center: true,
+    }
+  },
+  plugins: [
+],
+}
+
